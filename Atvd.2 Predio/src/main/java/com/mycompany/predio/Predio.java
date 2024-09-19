44cfccfe7ae9e9fe4134df8778d814aa04cb57d1
unknown <gustavo69gls@gmail.com>
2024-09-19 19:49:38 -0300

    Nova tarefa

---
+++ Atvd.2 Predio/src/main/java/com/mycompany/predio/Predio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.predio;
+
+/**
+ *
+ * @author gusta
+ */
+public class Predio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
